783b860d43c90bd6ef0da396e8f6ef1968b39f49
bjoern <b.irion@wtnet.de>
2026-06-02 10:02:29 +0200

    Initial commit

---
+++ src/main/java/org/example/recappokedexservice/RecapPokedexServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.recappokedexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapPokedexServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecapPokedexServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/recappokedexservice/RecapPokedexServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.recappokedexservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapPokedexServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
